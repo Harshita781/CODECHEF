3fdb7289ef34f3ed08c5021c33d97e05f7a691ee
Harshita781 <sharmaharshita431@gmail.com>
2022-06-18 18:40:20 +0530

    codechef question

---
+++ Increase_IQ.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	int chef_IQ=0;
+	chef_IQ=x+7;
+	if(chef_IQ>170)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
